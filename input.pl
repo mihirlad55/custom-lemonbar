af608146e4b44f35a29c838519f5bd2b05f66afd
Mihir Lad <mihirlad55@gmail.com>
2019-06-01 17:00:33 -0400

    Added checks for backlight and battery

--- input.pl
+++ input.pl
@@ -21,9 +21,7 @@ my $SYM_BAT_0 = " ";
 my $SYM_WIFI = " ";
 
 my $SYM_CLOCK = " ";
-
-my $SYM_CALENDAR = " ";
-
+my $SYM_CALENDAR = " "; 
 my $SYM_VOLUME_UP = " ";
 my $SYM_VOLUME_DOWN = " ";
 my $SYM_VOLUME_OFF = " ";
@@ -102,6 +100,17 @@ STDOUT->autoflush;
 
 #print 'passeed';
 
+my $HAS_BATTERY = 0;
+my $HAS_BACKLIGHT = 0;
+
+if (`if [ "\$(ls -A /sys/class/power_supply)" ]; then echo 1; else echo 0; fi` == 1) {
+    $HAS_BATTERY = 1;
+}
+
+if (`if [ "\$(ls -A /sys/class/backlight)" ]; then echo 1; else echo 0; fi` == 1) {
+    $HAS_BACKLIGHT = 1;
+}
+
 while (1) {
     my $time = lc `date +"%I:%M%p"`;
     chomp $time; # Remove new line character
@@ -109,33 +118,37 @@ while (1) {
     my $date = `date +"%a %b %d"`;
     chomp $date; # Remove new line character
 
-    my $bat = `cat /sys/class/power_supply/BAT0/charge_now` / `cat /sys/class/power_supply/BAT0/charge_full` * 100;
-    $bat = int $bat;
-
-    my $CHR = `cat /sys/class/power_supply/AC/online`;
-
     my $colorBat;
+    my $bat;
 
-    if ($CHR == 1) {
-        $bat = "$SYM_BAT_CHR $bat%";
-        $colorBat = $COLOR_POWER[0];
-    }
-    else {
-        if ( $bat == 100 ) {
-            $bat = "$SYM_BAT_FULL $bat%";
+    if ($HAS_BATTERY) {
+        $bat = `cat /sys/class/power_supply/BAT0/charge_now` / `cat /sys/class/power_supply/BAT0/charge_full` * 100;
+        $bat = int $bat;
+
+        my $CHR = `cat /sys/class/power_supply/AC/online`;
+
+
+        if ($CHR == 1) {
+            $bat = "$SYM_BAT_CHR $bat%";
             $colorBat = $COLOR_POWER[0];
-        } elsif ( $bat > 75 ) {
-            $bat = "$SYM_BAT_75 $bat%";
-            $colorBat = $COLOR_POWER[1];
-        } elsif ( $bat > 50 ) {
-            $bat = "$SYM_BAT_50 $bat%";
-            $colorBat = $COLOR_POWER[2];
-        } elsif ( $bat > 10 ) {
-            $bat = "$SYM_BAT_25 $bat%";
-            $colorBat = $COLOR_POWER[3];
-        } else {
-            $bat = "$SYM_BAT_0 $bat%";
-            $colorBat = $COLOR_POWER[4];
+        }
+        else {
+            if ( $bat == 100 ) {
+                $bat = "$SYM_BAT_FULL $bat%";
+                $colorBat = $COLOR_POWER[0];
+            } elsif ( $bat > 75 ) {
+                $bat = "$SYM_BAT_75 $bat%";
+                $colorBat = $COLOR_POWER[1];
+            } elsif ( $bat > 50 ) {
+                $bat = "$SYM_BAT_50 $bat%";
+                $colorBat = $COLOR_POWER[2];
+            } elsif ( $bat > 10 ) {
+                $bat = "$SYM_BAT_25 $bat%";
+                $colorBat = $COLOR_POWER[3];
+            } else {
+                $bat = "$SYM_BAT_0 $bat%";
+                $colorBat = $COLOR_POWER[4];
+            }
         }
     }
     
@@ -145,9 +158,11 @@ while (1) {
     my $ip = `ip addr show | grep -o 'inet.*brd' | cut -d ' ' -f 2 | cut -d '/' -f 1`;
     chomp $ip; # Remove new line character
 
-    my $ipText = "";
+    my $ipText = "$ipInterface:  $ip";
 
-    if ($ipInterface ne "") {
+    if ($ipInterface =~ m/enp/) {
+        $colorIP = $COLOR_STRENGTH[0];
+    } elsif ($ipInterface ne "") {
         my $ipQuality = `iw $ipInterface station dump | awk '/signal avg:/{print \$3; exit;}'`;
 
         if ( $ipQuality < -70 ) {
@@ -160,11 +175,9 @@ while (1) {
             $colorIP = $COLOR_STRENGTH[0];
         }
 
-        $ipText = "$ipInterface:  $ip";
     } else {
         $colorIP = $COLOR_STRENGTH[3];
     }
-        
 
     my $volume = `amixer get 'Master' | awk '/\\[(on)|(off)\\]/{print \$3; exit}' | tr -d '[]%\n'`;
 
@@ -178,29 +191,42 @@ while (1) {
 
     my $activeWindowTitle = `xprop -id \$(xprop -root 32x '\t\$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | sed 's/^_NET_WM_NAME(UTF8_STRING) = //g' | tr -d '""\n'`;
 
-    my $brightness = int `cat /sys/class/backlight/intel_backlight/brightness` / `cat /sys/class/backlight/intel_backlight/max_brightness` * 100;
-
     my $brightnessText;
-    if ($brightness == 100) {
-        $brightnessText = $SYM_BRIGHTNESS[2];
-    } elsif ($brightness >= 50) {
-        $brightnessText = $SYM_BRIGHTNESS[1];
-    } else {
-        $brightnessText = $SYM_BRIGHTNESS[0];
+    if ($HAS_BACKLIGHT) {
+        my $brightness = int `cat /sys/class/backlight/intel_backlight/brightness` / `cat /sys/class/backlight/intel_backlight/max_brightness` * 100;
+
+        my $brightnessText;
+        if ($brightness == 100) {
+            $brightnessText = $SYM_BRIGHTNESS[2];
+        } elsif ($brightness >= 50) {
+            $brightnessText = $SYM_BRIGHTNESS[1];
+        } else {
+            $brightnessText = $SYM_BRIGHTNESS[0];
+        }
+        $brightnessText .= "  $brightness%";
     }
-    $brightnessText .= "  $brightness%";
 
     my $blockTime = "%{F$COLOR_PRIMARY[0]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[0]}%{F$COLOR_SECONDARY[0]}  $SYM_CLOCK  $time ";
 
     my $blockDate = "%{F$COLOR_PRIMARY[1]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[1]}%{F$COLOR_SECONDARY[1]}   $SYM_CALENDAR   $date";
 
-    my $blockBat = "%{F$COLOR_PRIMARY[2]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[2]}%{F$colorBat}   $bat";
+
+    my $blockBat = "";
+    if ($HAS_BATTERY) {
+        $blockBat = "%{F$COLOR_PRIMARY[2]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[2]}%{F$colorBat}   $bat";
+    } else {
+    }
 
     my $blockIp = "%{F$COLOR_PRIMARY[3]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[3]}%{F$colorIP}  $SYM_WIFI  %{F$COLOR_SECONDARY[3]}$ipText" ;
 
     my $blockVolume = "%{F$COLOR_PRIMARY[4]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[4]}%{F$COLOR_SECONDARY[4]}  $volume";
     
-    my $blockBrightness = "%{F$COLOR_PRIMARY[5]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[5]}%{F$COLOR_SECONDARY[5]}  $brightnessText";
+    my $blockBrightness;
+    if ($HAS_BACKLIGHT) {
+        $blockBrightness = "%{F$COLOR_PRIMARY[5]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[5]}%{F$COLOR_SECONDARY[5]}  $brightnessText";
+    } else {
+        $blockBrightness = "";
+    }
 
     my $blockWindowTitle = "%{F$COLOR_PRIMARY[6]}$ARROW_LEFT_SOLID%{B$COLOR_PRIMARY[6]}%{F$COLOR_SECONDARY[6]} $activeWindowTitle";
 
